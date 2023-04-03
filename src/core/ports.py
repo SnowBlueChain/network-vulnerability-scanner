47e0587d764809057f32ff3154d75deb0a9b0cbf
FW661 <frankwong661@gmail.com>
2023-04-03 17:52:28 +0200

    fix: bug when no ports are open

--- src/core/ports.py
+++ src/core/ports.py
@@ -5,15 +5,17 @@ def scan_ports(ip: str) -> bool:
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     established = False
 
-    print("PORT\t  STATE")
-
     for port in range(1, 65535):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             try:
                 client_socket.settimeout(0.5)
                 client_socket.connect((ip, port))
+
+                if not established:
+                    print("PORT\t  STATE")
+                    established = True
+
                 print(f"{port}/tcp".ljust(9), "open")
-                established = True
             except:
                 pass
 
